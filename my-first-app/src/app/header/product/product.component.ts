7887e90bb2c9d5ab35451f6495be9deb8935b9e3
dhruvchavda05 <dhruvbchavda@gmail.com>
2022-02-25 09:33:16 +0530

    Shopping Website

--- my-first-app/src/app/header/product/product.component.ts
+++ my-first-app/src/app/header/product/product.component.ts
@@ -22,12 +22,66 @@ export class ProductComponent implements OnInit {
   }
 
   show1 = false;
-  
+  show2 = false;
+  show3 = false;
+  show4 = false;
+  show5 = false;
+  show6 = false;
+  show7 = false;
   showdesc(){
     if(this.show1==true){
       this.show1=false;
       return;
     }
     this.show1=true;
+  }
+  
+  showdesc2(){
+    
+    if(this.show2==true){
+      this.show2=false;
+      return;
+    }
+    this.show2=true;
+  }
+  showdesc3(){
+    
+    if(this.show3==true){
+      this.show3=false;
+      return;
+    }
+    this.show3=true;
+  }
+  showdesc4(){
+    
+    if(this.show4==true){
+      this.show4=false;
+      return;
+    }
+    this.show4=true;
+  }
+  showdesc5(){
+    
+    if(this.show5==true){
+      this.show5=false;
+      return;
+    }
+    this.show5=true;
+  }
+  showdesc6(){
+    
+    if(this.show6==true){
+      this.show6=false;
+      return;
+    }
+    this.show6=true;
+  }  
+  showdesc7(){
+    
+    if(this.show7==true){
+      this.show7=false;
+      return;
+    }
+    this.show7=true;
   }
 }
